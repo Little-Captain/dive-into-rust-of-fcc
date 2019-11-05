6ce02f56d6fe4540314fe727b7e40037c8a1ea26
Little-Captain <coder.littlecaptain@gmail.com>
2019-11-05 21:23:48 +0800

    ch02 doing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+#[allow(dead_code)]
+
 fn main1() {
     use dive_into_rust_of_fcc::ch01::first;
     first::hello_world();
     first::print();
 }
 
+fn main2() {
+    use dive_into_rust_of_fcc::ch02::first;
+    first::def_variable();
+    first::shadowing_var();
+    first::shadowing_var1();
+}
+
 fn main() {
-    main1();
+    main2();
 }
