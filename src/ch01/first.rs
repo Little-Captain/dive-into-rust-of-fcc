d3ddbbd9f4d80aa65c3a712774fc5d1e45c0bdb5
Little-Captain <coder.littlecaptain@gmail.com>
2019-11-05 19:00:43 +0800

    hello world

---
+++ src/ch01/first.rs
@@ -0,0 +1,4 @@
+pub fn hello_world() {
+    let s = "hello world!";
+    println!("{}", s);
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main1() {
+    use dive_into_rust_of_fcc::ch01::first;
+    first::hello_world();
+}
+
+fn main() {
+    main1();
+}
