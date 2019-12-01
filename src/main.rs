35d2a2acb88463701b77f48178bf15214443fa21
Little-Captain <coder.littlecaptain@gmail.com>
2019-12-01 17:51:30 +0800

    ch05 5.3 completed

--- src/main.rs
+++ src/main.rs
@@ -66,10 +66,12 @@ fn main4_1() {
 fn main5_1() {
     use dive_into_rust_of_fcc::ch05::first;
     use dive_into_rust_of_fcc::ch05::second;
+    use dive_into_rust_of_fcc::ch05::third;
     first::trait1();
     first::trait2();
     first::trait3();
     second::learn_static_method();
+    third::learn_extension();
 }
 
 fn main() {
